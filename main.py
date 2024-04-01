49cbd3accbff9c79a3e7606889bf044fc41a15de
legendaryleaks <160411614+legendaryleaks@users.noreply.github.com>
2024-04-01 11:25:31 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+HAPPY APRIL FOOLS
+YOU GOT SCAMMED
+
+i actually really liked this social experiment. nobody cared that i was getting sued and
+could lose it all but were happy to get this scanner. and as soon as i made the announcement
+everyone was asking for compensation lol. so gg. ur never getting scanner code!
+
+love Jack <3
